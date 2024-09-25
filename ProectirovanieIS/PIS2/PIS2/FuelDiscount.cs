7dcd8b44a9859726267a3a17cc9133b9ac2674fb
Moskoovskih <106261697+Moskoovskih@users.noreply.github.com>
2024-09-25 10:50:51 +0700

    Add files via upload

---
+++ ProectirovanieIS/PIS2/PIS2/FuelDiscount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS2
+{
+    internal class FuelDiscount
+    {
+        public string FuelType { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal DiscountPercent { get; set; }
+
+        public static FuelDiscount ParseFuelDiscount(string input)
+        {
+            
+            input = input.Trim();            
+            if (!input.StartsWith("\"") || !input.EndsWith("\""))
+                throw new ArgumentException("Некорректный формат входной строки");           
+            input = input.Substring(1, input.Length - 2);            
+            string[] parts = input.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 4)
+                throw new ArgumentException("Некорректный формат входной строки");
+            string fuelType = parts[0];          
+            if (DateTime.TryParseExact(parts[1], "yyyyMMdd", null, DateTimeStyles.None, out DateTime startDate) &&
+                DateTime.TryParseExact(parts[2], "yyyyMMdd", null, DateTimeStyles.None, out DateTime endDate))
+            {
+                int discountPercent = int.Parse(parts[3]);
+
+                return new FuelDiscount
+                {
+                    FuelType = fuelType,
+                    StartDate = startDate,
+                    EndDate = endDate,
+                    DiscountPercent = discountPercent
+                };
+            }
+            else
+            {
+                throw new FormatException("Некорректный формат даты в входной строке.");
+            }
+        }
+    }
+}
---
+++ ProectirovanieIS/PIS2/PIS2/FuelPrice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS2
+{
+    public class FuelPrice
+    {
+        public string FuelType { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Price { get; set; }
+
+        public static FuelPrice ParseFuelPrice(string input)
+        {
+            
+            input = input.Trim();            
+            if (!input.StartsWith("'") || !input.EndsWith("'"))
+                throw new ArgumentException("Некорректный формат входной строки");          
+            input = input.Substring(1, input.Length - 2);            
+            string[] parts = input.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 3)
+                throw new ArgumentException("Некорректный формат входной строки");
+            string fuelType = parts[0];
+            DateTime date = DateTime.ParseExact(parts[1], "yyyy.MM.dd", null);          
+            if (decimal.TryParse(parts[2], NumberStyles.Any, CultureInfo.InvariantCulture, out decimal price))
+            {
+                return new FuelPrice
+                {
+                    FuelType = fuelType,
+                    Date = date,
+                    Price = price
+                };
+            }
+            else
+            {
+                throw new FormatException($"Невозможно преобразовать '{parts[2]}' в десятичное число.");
+            }
+        }
+    }
+}
